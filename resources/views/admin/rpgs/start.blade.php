f6e5044477ff6e2e0c4fbcc21b90b2a0565d685e
Adri Silva <adrisilva0002@gmail.com>
2019-04-14 19:34:53 -0300

    estrutura modal details

--- resources/views/admin/rpgs/start.blade.php
+++ resources/views/admin/rpgs/start.blade.php
@@ -167,45 +167,85 @@
 
                             <li class=""><a href="#tab_2" data-toggle="tab" aria-expanded="false">Atributos</a></li>
 
-                            <li class="" {!! $controlRpg ? 'style="display: block"' : 'style="display: none"' !!} id="inventory">
+                            <li class="disabled" {!! $controlRpg ? 'style="display: block"' : 'style="display: none"' !!} id="inventory">
                                 <a href="#tab_3" data-toggle="tab" aria-expanded="true">Inventário</a></li>
 
-                            <li class="" {!! $controlRpg ? 'style="display: block"' : 'style="display: none"' !!} id="spells">
-                                <a href="#tab_3" data-toggle="tab" aria-expanded="true">Magias</a></li>
+                            <li class="disabled" {!! $controlRpg ? 'style="display: block"' : 'style="display: none"' !!} id="spells">
+                                <a href="#tab_4" data-toggle="tab" aria-expanded="true">Magias</a></li>
                         </ul>
                         <div class="tab-content">
                             <div class="tab-pane" id="tab_1">
-                                <b>How to use:</b>
-
-                                <p>Exactly like the original bootstrap tabs except you should use
-                                    the custom wrapper <code>.nav-tabs-custom</code> to achieve this style.</p>
-                                A wonderful serenity has taken possession of my entire soul,
-                                like these sweet mornings of spring which I enjoy with my whole heart.
-                                I am alone, and feel the charm of existence in this spot,
-                                which was created for the bliss of souls like mine. I am so happy,
-                                my dear friend, so absorbed in the exquisite sense of mere tranquil existence,
-                                that I neglect my talents. I should be incapable of drawing a single stroke
-                                at the present moment; and yet I feel that I never was a greater artist than now.
+                                <p>
+                                    <strong>Jogador: </strong> <span id="player_name">Teste</span>
+                                </p>
+                                <p>
+                                    <strong>Nome: </strong> <span id="char_name">Teste</span>
+                                </p>
+                                <p>
+                                    <strong>Raça: </strong> <span id="race">Teste</span>
+                                </p>
+                                <p>
+                                    <strong>Sub-Raça: </strong> <span id="sub_race"> Teste</span>
+                                </p>
+                                <p>
+                                    <strong>Classe: </strong> <span id="class">Teste</span>
+                                </p>
+                                <p>
+                                    <strong>Descrição: </strong> <span id="description">Teste</span>
+                                </p>
                             </div>
                             <!-- /.tab-pane -->
                             <div class="tab-pane" id="tab_2">
-                                The European languages are members of the same family. Their separate existence is a myth.
-                                For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ
-                                in their grammar, their pronunciation and their most common words. Everyone realizes why a
-                                new common language would be desirable: one could refuse to pay expensive translators. To
-                                achieve this, it would be necessary to have uniform grammar, pronunciation and more common
-                                words. If several languages coalesce, the grammar of the resulting language is more simple
-                                and regular than that of the individual languages.
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Atributo</th>
+                                            <th>Valor</th>
+                                            <th>Modificador</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Força</td>
+                                            <td id="force_value">14</td>
+                                            <td id="force_modify">+2</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Destreza</td>
+                                            <td id="skill_value">14</td>
+                                            <td id="skill_modify">+2</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Constituição</td>
+                                            <td id="constitution_value">14</td>
+                                            <td id="constitution_modify">+2</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Sabedoria</td>
+                                            <td id="sapience_value">14</td>
+                                            <td id="sapience_modify">+2</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Carisma</td>
+                                            <td id="charisma_value">14</td>
+                                            <td id="charisma_modify">+2</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Inteligência</td>
+                                            <td id="intelligence_value">14</td>
+                                            <td id="intelligence_modify">+2</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
                             <!-- /.tab-pane -->
                             <div class="tab-pane active" id="tab_3">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                                It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-                                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-                                like Aldus PageMaker including versions of Lorem Ipsum.
+
+                            </div>
+                            <!-- /.tab-pane -->
+                            <!-- /.tab-pane -->
+                            <div class="tab-pane active" id="tab_4">
+
                             </div>
                             <!-- /.tab-pane -->
                         </div>
